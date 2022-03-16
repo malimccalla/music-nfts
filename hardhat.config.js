ea152dd26e23c3c491a975f8ef9764b89c90a760
Mali McCalla <malimccalla@gmail.com>
2022-03-16 12:38:39 +0000

    add mumbai and mainnet config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 
+const fs = require('fs');
+const privateKey = fs.readFileSync('.secret').toString()
+const projectId = '381a2b6065d24abb80b796130d7a9073';
+ 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 // task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +22,18 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.8.4",
+
+  networks: {
+    hardhat: {
+      chainId:  31337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mumbai.mainnet.io/v3/${projectId}`,
+      accounts: [privateKey]
+    } 
+  }
 };

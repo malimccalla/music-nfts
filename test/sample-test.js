530d3e6c5bc23c768b2385a17c9f804f47e12487
Mali McCalla <malimccalla@gmail.com>
2022-03-18 10:58:56 +0000

    Make tokens more front end friendly

--- test/sample-test.js
+++ test/sample-test.js
@@ -18,18 +18,31 @@ describe("NFTMarket", function () {
 
     const auctionPrice = ethers.utils.parseUnits('100', 'ether');
 
-    await nft.createToken("https://example.com/1");    
-    await nft.createToken("https://example.com/2");
+    const nft1 = await nft.createToken("https://example.com/1");    
+    const nft2 = await nft.createToken("https://example.com/2");
     
     await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: listingPrice });
     await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: listingPrice });
-    
 
     const [_, buyerAddress ] = await ethers.getSigners();
      
     await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice });
 
-    const items = await market.getMarketItems();
+    let items = await market.getMarketItems();
+
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+
+      const item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+
+      return item;
+    }))
 
     console.log('items: ', items)
 

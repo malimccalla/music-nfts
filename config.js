a29e22e09db87d4ef5ec476230b14da3312b1212
Mali McCalla <malimccalla@gmail.com>
2022-03-18 16:46:50 +0000

    Add deploy script

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftAddress = "";
-export const nftMarketAddress = "";
+export const nftAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const nftMarketAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
